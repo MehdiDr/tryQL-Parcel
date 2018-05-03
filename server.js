d2360e2bd593e7cf5290ec040dbf5e7e66e2c8cc
Mehdi Druon <mehdidruon@gmail.com>
2018-05-03 21:31:41 +0200

    Add server.

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+import { graphqlExpress, graphiqlExpress } from 'graphql-server-express';
+import bodyParser from 'body-parser';
+
+import { schema } from './src/schema';
+
+const PORT = 5000;
+const server = express();
+
+server.use('*', cors({ origin: 'http://localhost:8000' }));
+server.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+server.use('/graphiql', graphiqlExpress({ endpointURL: 'graphql' }));
+
+server.listen(PORT, () => console.log(`Ton server tourne sur le port ${PORT}`));
